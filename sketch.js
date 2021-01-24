745498a30b3bf91c2f710a08c008ce9c4ad6dc55
TECH VDD <srd.kmv@gmail.com>
2021-01-24 18:52:39 +0530

    Realtime DataBase

--- sketch.js
+++ sketch.js
@@ -1,29 +1,39 @@
 var ball;
-
+var dataBase,position
 function setup(){
     createCanvas(500,500);
+    dataBase=firebase.database();
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    
+    dataBase.ref("Ball").on("value",readPosition)
+}
+function readPosition(data){
+    position=data.val();
+    ball.x=position.x
+    ball.y=position.y
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    dataBase.ref("Ball").set({
+        x:position.x+x,
+        y:position.y+y
+    })
 }
